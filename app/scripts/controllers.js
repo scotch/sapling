ca4b2a4a39232cb81c0b2af91f00e20ad04f6cb6
DjebbZ <khalid.jebbari@gmail.com>
2012-09-03 17:20:49 +0200

    Converted app/scripts/controllers.coffee to js.

---
+++ app/scripts/controllers.js
@@ -0,0 +1,48 @@
+// Define the application level controllers
+angular.module('app.controllers', [])
+  // the AppCtrl is used in index.html (see app/assets/index.jade)
+  .controller('AppCtrl',
+    ['$scope',
+      '$location',
+      '$resource',
+      '$rootScope',
+
+      function ($scope, $location, $resource, $rootScope) {
+        // Uses the url to determine if the selected
+        // menu item should have the class active.
+        $scope.$location = $location;
+        $scope.$watch('$location.path()', function (path) {
+            $scope.activeNavId = path || '/';
+          }
+        );
+
+        // getClass compares the current url with the id.
+        // If the current url starts with the id it returns 'active'
+        // otherwise it will return '' an empty string. E.g.
+        //
+        //   # current url = '/products/1'
+        //  getClass('/products') # returns 'active'
+        //  getClass('/orders') # returns ''
+        $scope.getClass = function (id) {
+          if($scope.activeNavId.substring(0, id.length) == id)
+            return 'active';
+          else
+            return '';
+        }
+      }
+  ])
+
+  // Just placeholders controllers. To be filled with your own logic.
+  .controller('MyCtrl1', [
+    '$scope',
+    function ($scope) {
+      return $scope;
+    }
+  ])
+
+  .controller('MyCtrl2', [
+    '$scope',
+    function ($scope) {
+      return $scope;
+    }
+  ]);

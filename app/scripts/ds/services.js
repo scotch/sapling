5a9989e6be703cd17b5d855b60d28b4b9958daf0
DjebbZ <khalid.jebbari@gmail.com>
2012-09-05 12:07:48 +0200

    Converted app/scripts/ds/services.coffee to js.

---
+++ app/scripts/ds/services.js
@@ -0,0 +1,20 @@
+angular.module('ds.services', [
+  'config.services',
+  'rpc.services'
+])
+
+  .factory('user', [
+    'config',
+    '$rootScope',
+    'rpc',
+
+    function (cnfg, $rootScope, rpc) {
+      var kinds = {};
+      var stores = {};
+
+      return {
+        Register: function (kind, useLocal, useServer) {},
+        NewKey: function (kind, stringID) {}
+      }
+    }
+  ]);

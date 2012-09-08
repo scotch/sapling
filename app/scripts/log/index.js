5d060ae65b31432540f932692f7c6aa19f22275e
Kyle Finley <kyle.finley@gmail.com>
2012-09-08 18:06:55 -0500

    log: added functions

---
+++ app/scripts/log/index.js
@@ -0,0 +1,7 @@
+/**
+ * log module provides a logging services.
+ */
+
+// This module is a added as a convenience.
+// The convention is to include all sub-modules that are present in the directory.
+angular.module('log', ['log.services']);
---
+++ app/scripts/log/services.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/**
+ * log.service provides a logging service.
+ *
+ * log extend the angular service `$log` by adding the `assert` method,
+ * which will raise an error. It also, provides a way to silence errors,
+ * by setting the config constant LOG_LEVEL in config module.
+ *
+ * possible values: 'disable' || 'assert' || 'error' || 'warn' || 'info' || 'debug'
+ */
+
+angular.module('log.services', [
+  'config.services'
+])
+
+  .factory('log', [
+    'config',
+    '$log',
+
+    function (config, $log) {
+
+      var logMap = {
+        'disable': 0,
+        'assert': 1,
+        'error': 2,
+        'warn': 3,
+        'info': 2,
+        'debug': 4
+      };
+
+      function consoleLog(type) {
+        return function() {
+          if (logMap[type] >= logMap[config.LOG_LEVEL]) {
+            if (type === 'assert') {
+              if (!arguments[0]) {
+                new Error('assertion failed: ' + arguments[1]);
+              }
+            } else {
+              $log[type](arguments[0]);
+            }
+          }
+        };
+      }
+
+      return {
+        log:    consoleLog('log'),
+        warn:   consoleLog('warn'),
+        info:   consoleLog('info'),
+        error:  consoleLog('error'),
+        assert: consoleLog('assert')
+      };
+    }
+  ]);

c37a5da859e32b8ca5698583e39b4a1db1b5162c
Kyle Finley <kyle.finley@gmail.com>
2012-09-09 19:45:47 -0500

    session: started

---
+++ app/scripts/session/index.js
@@ -0,0 +1,7 @@
+/**
+ * session module provides a session services.
+ */
+
+// This module is a added as a convenience.
+// The convention is to include all sub-modules that are present in the directory.
+angular.module('session', ['session.services']);

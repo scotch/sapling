8d83f9b0b734e4e38d44d4bb4f122b19310be8a8
Kyle Finley <kyle.finley@gmail.com>
2012-09-11 08:17:35 -0500

    app: moved roc wait action to app.controllers.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,34 +2,45 @@
 
 // Define the application level controllers
 angular.module('app.controllers', [])
-  // the AppCtrl is used in index.html (see app/assets/index.jade)
-  .controller('AppCtrl',
-    ['$scope',
-      '$location',
+  // the AppCtrl is used in index.html (see app/assets/index.html)
+  .controller('AppCtrl', [
+    '$scope',
+    '$location',
 
-      function ($scope, $location) {
-        // Uses the url to determine if the selected
-        // menu item should have the class active.
-        $scope.$location = $location;
-        $scope.$watch('$location.path()', function (path) {
-            $scope.activeNavId = path || '/';
-          }
-        );
+    function ($scope, $location) {
 
-        // getClass compares the current url with the id.
-        // If the current url starts with the id it returns 'active'
-        // otherwise it will return '' an empty string. E.g.
-        //
-        //   # current url = '/products/1'
-        //  getClass('/products') # returns 'active'
-        //  getClass('/orders') # returns ''
-        $scope.getClass = function (id) {
-          if($scope.activeNavId.substring(0, id.length) == id)
-            return 'active';
-          else
-            return '';
+      // TODO moved these to directives
+
+      // 'rpc.status' is a channel used to broadcast messages. It's defined in rpc/services.js
+      $scope.$on('rpc.status', function (e, d) {
+        if (d === 'waiting')
+          $scope.WaitText = 'Working...';
+        else
+          $scope.WaitText = false;
+      });
+
+      // Uses the url to determine if the selected
+      // menu item should have the class active.
+      $scope.$location = $location;
+      $scope.$watch('$location.path()', function (path) {
+          $scope.activeNavId = path || '/';
         }
+      );
+
+      // getClass compares the current url with the id.
+      // If the current url starts with the id it returns 'active'
+      // otherwise it will return '' an empty string. E.g.
+      //
+      //   # current url = '/products/1'
+      //  getClass('/products') # returns 'active'
+      //  getClass('/orders') # returns ''
+      $scope.getClass = function (id) {
+        if($scope.activeNavId.substring(0, id.length) == id)
+          return 'active';
+        else
+          return '';
       }
+    }
   ])
 
   // Just placeholders controllers. To be filled with your own logic.

8b4bc82e2b8f225b69d5a818e4dbfbc0654aa2bc
Kyle Finley <kyle.finley@gmail.com>
2012-10-23 14:48:44 -0500

    Improved testing utility for mongoose

---
+++ server/test/utils/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/*
+ * Modified from https://github.com/elliotf/mocha-mongoose
+ */
+
+var config = require('../../config');
+var mongoose = require('mongoose');
+
+// ensure the NODE_ENV is set to 'test'
+// this is helpful when you would like to change behavior when testing
+process.env.NODE_ENV = 'test';
+
+
+beforeEach(function (done) {
+
+  function clearDB() {
+    for (var i in mongoose.connection.collections) {
+      mongoose.connection.collections[i].remove();
+    }
+    return done();
+  }
+
+  function reconnect() {
+    mongoose.connect(config.db.test, function (err) {
+      if (err) {
+        throw err;
+      }
+      return clearDB();
+    });
+  }
+
+  function checkState() {
+    switch (mongoose.connection.readyState) {
+    case 0:
+      reconnect();
+      break;
+    case 1:
+      clearDB();
+      break;
+    default:
+      process.nextTick(checkState);
+    }
+  }
+
+  checkState();
+});
+
+
+afterEach(function (done) {
+  mongoose.disconnect();
+  return done();
+});
